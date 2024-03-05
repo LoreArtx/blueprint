50fccae693e87a568ec19907d1debf5fe4534fe8
Danylo Dziubchuk <vt211_ddr@student.ztu.edu.ua>
2024-03-05 23:38:00 +0200

    initialised Button components

---
+++ components/UI/Button/index.tsx
@@ -0,0 +1,15 @@
+import { cva } from "class-variance-authority";
+import { FC } from "react";
+import classes from "./Button.module.scss"
+
+interface ButtonProps {
+    children: React.ReactNode
+}
+
+const buttonVariants = cva("")
+
+const Button: FC<ButtonProps> = ({ children }) => {
+    return <button>{children}</button>;
+}
+
+export default Button;
