ba531ff64a3b2e412666e97fe96f9e0d47fbf605
Danylo Dziubchuk <vt211_ddr@student.ztu.edu.ua>
2024-03-14 03:48:00 +0200

    actions with nextauth

--- app/profile/settings/page.tsx
+++ app/profile/settings/page.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import SignOutButton from './components/SignOutButton'
 
 const ProfileSettingsPage = () => {
     return (
-        <div>ProfileSettingsPage</div>
+        <SignOutButton />
     )
 }
 
--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,9 +1,91 @@
-import React from 'react'
+"use client"
 
-const SigninPage = () => {
+import React from 'react'
+import { signInByGithub } from '../utils/signin'
+import { useSearchParams } from 'next/navigation'
+
+
+const SignInPage = () => {
+    const searchParams = useSearchParams()
+    let callbackUrl: any = searchParams.get("callbackUrl")
     return (
-        <div>SigninPage</div>
+        <section className="h-screen">
+            <div className="container h-full px-6 py-24">
+                <div
+                    className="flex h-full flex-wrap items-center justify-center lg:justify-between">
+                    <div className="mb-12 md:mb-0 md:w-8/12 lg:w-6/12">
+                        <img
+                            src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+                            className="w-full"
+                            alt="Phone image" />
+                    </div>
+
+                    <div className="md:w-8/12 lg:ms-6 lg:w-5/12">
+                        <form>
+                            <div className="relative mb-6" data-twe-input-wrapper-init>
+                                <input
+                                    type="text"
+                                    className="peer block min-h-[auto] w-full rounded border-2 border-slate-600 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[twe-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary [&:not([data-twe-input-placeholder-active])]:placeholder:opacity-0"
+                                    id="exampleFormControlInput3"
+                                    placeholder="Email address" />
+                                <label
+                                    htmlFor="exampleFormControlInput3"
+                                    className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[2rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[twe-input-state-active]:-translate-y-[2rem] peer-data-[twe-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-400 dark:peer-focus:text-primary"
+                                >Email address
+                                </label>
+                            </div>
+
+                            <div className="relative mb-6" data-twe-input-wrapper-init>
+                                <input
+                                    type="password"
+                                    className="peer block min-h-[auto] w-full rounded border-2 border-slate-600 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[twe-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary [&:not([data-twe-input-placeholder-active])]:placeholder:opacity-0"
+                                    id="exampleFormControlInput33"
+                                    placeholder="Password" />
+                                <label
+                                    htmlFor="exampleFormControlInput33"
+                                    className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[2rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[twe-input-state-active]:-translate-y-[2rem] peer-data-[twe-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-400 dark:peer-focus:text-primary"
+                                >Password
+                                </label>
+                            </div>
+
+                            <div className="mb-6 flex items-center justify-end">
+                                <a
+                                    href="#!"
+                                    className="text-blue-500 focus:outline-none dark:text-primary-400"
+                                >Forgot password?</a>
+                            </div>
+
+                            <button
+                                type="submit"
+                                className="inline-block w-full rounded bg-blue-700 px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-milk shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
+                                data-twe-ripple-init
+                                data-twe-ripple-color="light">
+                                Sign in
+                            </button>
+
+                            <div
+                                className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300 dark:before:border-neutral-500 dark:after:border-neutral-500">
+                                <p
+                                    className="mx-4 mb-0 text-center font-semibold dark:text-neutral-200">
+                                    OR
+                                </p>
+                            </div>
+
+                            <button
+                                onClick={() => signInByGithub({ callbackUrl })}
+                                type="button" className="py-2 px-4 flex justify-center items-center bg-gray-600 hover:bg-gray-700 focus:ring-gray-500 focus:ring-offset-gray-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="mr-2" viewBox="0 0 1792 1792">
+                                    <path d="M896 128q209 0 385.5 103t279.5 279.5 103 385.5q0 251-146.5 451.5t-378.5 277.5q-27 5-40-7t-13-30q0-3 .5-76.5t.5-134.5q0-97-52-142 57-6 102.5-18t94-39 81-66.5 53-105 20.5-150.5q0-119-79-206 37-91-8-204-28-9-81 11t-92 44l-38 24q-93-26-192-26t-192 26q-16-11-42.5-27t-83.5-38.5-85-13.5q-45 113-8 204-79 87-79 206 0 85 20.5 150t52.5 105 80.5 67 94 39 102.5 18q-39 36-49 103-21 10-45 15t-57 5-65.5-21.5-55.5-62.5q-19-32-48.5-52t-49.5-24l-20-3q-21 0-29 4.5t-5 11.5 9 14 13 12l7 5q22 10 43.5 38t31.5 51l10 23q13 38 44 61.5t67 30 69.5 7 55.5-3.5l23-4q0 38 .5 88.5t.5 54.5q0 18-13 30t-40 7q-232-77-378.5-277.5t-146.5-451.5q0-209 103-385.5t279.5-279.5 385.5-103zm-477 1103q3-7-7-12-10-3-13 2-3 7 7 12 9 6 13-2zm31 34q7-5-2-16-10-9-16-3-7 5 2 16 10 10 16 3zm30 45q9-7 0-19-8-13-17-6-9 5 0 18t17 7zm42 42q8-8-4-19-12-12-20-3-9 8 4 19 12 12 20 3zm57 25q3-11-13-16-15-4-19 7t13 15q15 6 19-6zm63 5q0-13-17-11-16 0-16 11 0 13 17 11 16 0 16-11zm58-10q-2-11-18-9-16 3-14 15t18 8 14-14z"></path>
+                                </svg>
+                                Sign in with GitHub
+                            </button>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
-export default SigninPage
+export default SignInPage
