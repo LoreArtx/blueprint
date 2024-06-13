a47445dc3b51f92d0af9b82d998dcb3a3027b7bb
Danylo Dziubchuk <vt211_ddr@student.ztu.edu.ua>
2024-06-13 04:20:52 +0300

    added form to create project

--- frontend/app/(default)/projects/create/page.tsx
+++ frontend/app/(default)/projects/create/page.tsx
@@ -1,9 +1,98 @@
-import React from 'react'
+"use client"
 
-const CreateProjectPage = () => {
+import React from 'react';
+import ICriteria from '@/types/ICriteria';
+import useForm from '@/hooks/useForm';
+import { showToast } from '@/app/utils/showToast';
+import usePostData from '@/hooks/usePostData';
+import IBlueprint from '@/types/IBlueprint';
+import { useSession } from 'next-auth/react';
+import { Button, Input, Select, Textarea } from '@/components/UI';
+
+const CreateProjectPage: React.FC = () => {
+    const initialValues = {
+        title: '',
+        deadline: '',
+        description: '',
+        privacy: 'public' as 'public' | 'private'
+    };
+
+    const { values, handleChange, setFormValues } = useForm(initialValues);
+    const { title, deadline, description, privacy } = values;
+    const { error, postData } = usePostData<IBlueprint>('http://localhost:5555/api/blueprints/create');
+    const { data: session } = useSession()
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault()
+        if (!title || !deadline || !description) {
+            showToast("error", "Inputs can't be empty")
+            return
+        }
+
+        if (new Date() > new Date(deadline)) {
+            showToast("error", "Deadline must be for at least today")
+            return
+        }
+
+        if (error)
+            showToast("error", error)
+
+        values.deadline = new Date(deadline).toISOString()
+        //@ts-ignore
+        await postData({ ...values, creatorID: session?.user.dbID })
+
+        showToast("success", "You did it!")
+    }
+
     return (
-        <div>CreateProjectPage</div>
-    )
-}
+        <div className="container mx-auto p-4 pt-[80px]">
+            <form onSubmit={handleSubmit} className="grid grid-cols-12 gap-2">
+                <div className='col-span-8 p-2'>
+                    <Input
+                        type="text"
+                        label="Project Title"
+                        name='title'
+                        placeholder=''
+                        value={title}
+                        onChange={handleChange}
+                    />
+                    <Textarea
+                        label="Description"
+                        name="description"
+                        value={description}
+                        onChange={handleChange}
+                        required />
+                    <div className='grid grid-cols-2'>
+                        <Input
+                            type="date"
+                            name='deadline'
+                            placeholder=''
+                            label="Deadline"
+                            value={deadline}
+                            onChange={handleChange}
+                            required
+                        />
+                        <Select
+                            label="Privacy"
+                            value={privacy}
+                            id="privacy"
+                            onChange={handleChange}
+                            options={[
+                                { value: 'public', label: 'Public' },
+                                { value: 'private', label: 'Private' }
+                            ]}
+                        />
+                    </div>
+                </div>
+                <div className='col-span-4 row-span-2 bg-white rounded shadow flex justify-center items-center'>
+                    <h1 className="text-2xl font-bold mb-4">Create New Project</h1>
+                </div>
+                <div className='col-span-8 row-start-2'>
+                    <Button type="submit">Create Project</Button>
+                </div>
+            </form>
+        </div>
+    );
+};
 
-export default CreateProjectPage
+export default CreateProjectPage;
