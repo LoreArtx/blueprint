c44e25f034196fa5985988df295356bc34f63cab
Danylo Dziubchuk <vt211_ddr@student.ztu.edu.ua>
2024-06-13 04:17:42 +0300

    updated hooks for server communication

---
+++ frontend/hooks/usePostData.ts
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+
+interface PostDataResult<T> {
+    error: string | null;
+    postData: (body:any)=>Promise<void>
+}
+
+const usePostData = <T>(url: string): PostDataResult<T> => {
+    const [error, setError] = useState<string | null>(null);
+
+    const postData = async(body: any)=>{
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(
+                     Object.entries(body).reduce((acc, [key, value]) => {
+                        const titleCaseKey = key.replace(/\b\w/g, (char) => char.toUpperCase());
+                        acc[titleCaseKey] = value;
+                        return acc;
+                     }, {} as Record<string, any>)
+                ),
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to post data!');
+            }
+
+        } catch (error: any) {
+            setError(error.message || 'An error occurred');
+        }
+        }
+
+    return { error, postData };
+};
+
+export default usePostData;
