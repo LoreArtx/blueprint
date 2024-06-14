6915dfdbc9179b3e83f841fe227dcb3e3dba565a
Danylo Dziubchuk <vt211_ddr@student.ztu.edu.ua>
2024-06-15 01:00:08 +0300

    Adapted displaying project info with project provider

--- frontend/app/(default)/projects/[id]/components/ProjectFiles.tsx
+++ frontend/app/(default)/projects/[id]/components/ProjectFiles.tsx
@@ -3,23 +3,23 @@
 import React from 'react';
 import Button from '@/components/UI/Button';
 import RepositoryInfo from './RepositoryInfo';
+import Link from 'next/link';
+import { useProject } from '@/components/Providers/ProjectProvider';
 
-interface ProjectFilesProps {
-    title: string;
-    deadline: string;
-}
 
-const ProjectFiles: React.FC<ProjectFilesProps> = ({ title, deadline }) => {
-    return (
-        <div className="bg-white p-4 rounded shadow h-full">
-            <div className='grid grid-cols-4'>
-                <h1 className="text-3xl font-bold col-span-2">{title}</h1>
-                {/* <div className='col-start-4 float-right'><Button>Add File</Button></div> */}
+
+const ProjectFiles: React.FC = () => {
+    const { project, amICreator } = useProject()
+    return project &&
+        <div className="bg-white p-4 rounded shadow-lg h-full">
+            <div className='grid grid-cols-4 mb-4'>
+                <h1 className="text-3xl font-bold col-span-2">{project.title}</h1>
+                {amICreator && <div className='col-start-4'><Link href={window.location + "/settings"}><Button>Settings</Button></Link></div>}
             </div>
 
             <RepositoryInfo></RepositoryInfo>
-        </div>
-    );
+        </div >
+        ;
 };
 
 export default ProjectFiles;
--- frontend/app/(default)/projects/[id]/page.tsx
+++ frontend/app/(default)/projects/[id]/page.tsx
@@ -1,31 +1,16 @@
-//@ts-nocheck
-"use client"
-
-import useFetchData from '@/hooks/useFetchData';
-import IBlueprint from '@/types/IBlueprint';
-import { useSession } from 'next-auth/react';
-import { useParams } from 'next/navigation';
 import React from 'react'
 import ProjectFiles from './components/ProjectFiles';
 import Description from './components/Description';
 import Progress from './components/Progress';
-import Criterias from './components/Criterias';
+import Criterias from './components/Criteria/Criterias';
 
 const ProjectPage = () => {
-    const params = useParams()
-    const session = useSession()
-
-
-    const { data: project, error } = useFetchData<IBlueprint | null>(`/blueprints/${session.data?.user.dbID}/${params.id}`);
-    const amICreator = project?.creatorId === session.data?.user.dbID
-
-
     return (
-        project && <div className="container mx-auto p-4 grid grid-cols-12 grid-rows-8 gap-4">
-            <div className='col-span-8 row-span-8'><ProjectFiles title={project.title} deadline={project.deadline} /></div>
-            <div className='col-span-4 row-span-1'><Description description={project.description} /></div>
-            <div className='col-span-4 row-span-1 '>{project.criterias.length > 0 && <Progress project={project} />}</div>
-            <div className='col-span-4 row-span-6'><Criterias project={project} moderate={amICreator} /></div>
+        <div className="container mx-auto p-4 grid grid-cols-12 grid-rows-7 gap-4">
+            <div className='col-span-8 row-span-7'><ProjectFiles /></div>
+            <div className='col-span-4 row-span-1'><Description /></div>
+            <div className='col-span-4 row-span-1 bg-white p-4 rounded shadow-lg h-full'><Progress /></div>
+            <div className='col-span-4 row-span-5'><Criterias /></div>
         </div>
     );
 }
